50d37d7286783728bdc6ce84b7593175530e1576
sendipatryjar <sendi.patryjar@lakuemas.com>
2024-12-29 16:18:11 +0700

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({ children }: { children?: React.ReactNode }) {
+ 
   return <ThemeProvider>{children}</ThemeProvider>;
 }
 
